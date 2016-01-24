0451f31e6dd55063689742dcb103ffffb2b3170b
Mudassar Hashmi <mudassarziahashmim@hotmail.com>
2016-01-24 05:08:55 -0700

    Showing Regression Table

--- Algos/LinearRegression.py
+++ Algos/LinearRegression.py
@@ -9,10 +9,11 @@ x = np.array([112, 345, 190, 305, 372, 550, 302, 420, 570])
 # Prizes of houses
 y = np.array([1120, 1523, 2102, 2230, 2600, 3200, 3409, 3689, 4460])
 
-slope, intercept, r_value, p_value, std_arr = stats.linregress(x, y)
+slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
 plt.plot(x, y, 'ro', color='black')
-plt.xlabel('Prices of Houses')
-plt.ylabel('Sizes of Houses')
+plt.xlabel('Sizes of Houses')
+plt.ylabel('Prices of Houses')
 
-plt.axis(0, 600, 0, 8000)
+plt.axis([0, 600, 0, 5000])
 plt.plot()
+plt.show()
