8f6398200db593fd9beb91bf92c42bb920b31a52
Mudassar Hashmi <mudassarziahashmim@hotmail.com>
2016-01-24 04:23:04 -0700

    Regression | Linear

--- Algos/LinearRegression.py
+++ Algos/LinearRegression.py
@@ -1,1 +1,18 @@
 __author__ = 'Mudassar'
+
+from scipy import stats
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Size of the houses
+x = np.array([112, 345, 190, 305, 372, 550, 302, 420, 570])
+# Prizes of houses
+y = np.array([1120, 1523, 2102, 2230, 2600, 3200, 3409, 3689, 4460])
+
+slope, intercept, r_value, p_value, std_arr = stats.linregress(x, y)
+plt.plot(x, y, 'ro', color='black')
+plt.xlabel('Prices of Houses')
+plt.ylabel('Sizes of Houses')
+
+plt.axis(0, 600, 0, 8000)
+plt.plot()
